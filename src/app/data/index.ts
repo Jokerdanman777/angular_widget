7bd61833d62590c2243c6b59c4a2906c43a419a3
Kudashov <kudashov@kursksu.ru>
2017-11-21 12:16:49 +0300

    Изменен путь для images

--- src/app/data/index.ts
+++ src/app/data/index.ts
@@ -5,16 +5,16 @@ const data: Hotel[]  = [
     {
     address: '35 Boston Street',
     phone:  '5461815489',   // дополнительно задание pipe для форматирования
-    img: '/assets/images/res.jpg',
+    img: 'assets/images/res.jpg',
     weather: {
        title: 'Boston',
-       img: '/assets/images/tstorms.png',
+       img: 'assets/images/tstorms.png',
        water: 19,
        temperatur: 33
     },
     social_info: {
        title: '@Boston',
-       img: '/assets/images/res.jpg',
+       img: 'assets/images/res.jpg',
        followers: 25000,
        following: 32000
     },
@@ -23,16 +23,16 @@ const data: Hotel[]  = [
   {
     address: 'Hollywood Boulevard‎',
     phone:  '6489376495',   // дополнительно задание pipe для форматирования
-    img: '/assets/images/r1.jpg',
+    img: 'assets/images/r1.jpg',
     weather: {
        title: 'LA',
-       img: '/assets/images/53565.png',
+       img: 'assets/images/53565.png',
        water: 30,
        temperatur: 36
     },
     social_info: {
        title: '@LA',
-       img: '/assets/images/r1.jpg',
+       img: 'assets/images/r1.jpg',
        followers: 100000,
        following: 250000
     },
@@ -41,16 +41,16 @@ const data: Hotel[]  = [
   {
     address: '150 California Street',
     phone:  '5846859548',   // дополнительно задание pipe для форматирования
-    img: '/assets/images/res.jpg',
+    img: 'assets/images/res.jpg',
     weather: {
        title: 'California',
-       img: '/assets/images/cloudy2.png',
+       img: 'assets/images/cloudy2.png',
        water: 29,
        temperatur: 35
     },
     social_info: {
        title: '@California',
-       img: '/assets/images/res.jpg',
+       img: 'assets/images/res.jpg',
        followers: 99000,
        following: 19900
     },
@@ -59,7 +59,7 @@ const data: Hotel[]  = [
   {
     address: '1312 Delaware St',
     phone:  '1235258495',   // дополнительно задание pipe для форматирования
-    img: '/assets/images/r1.jpg',
+    img: 'assets/images/r1.jpg',
     weather: {
        title: 'Delaware',
        img: '/assets/images/tstorms.png',
@@ -68,7 +68,7 @@ const data: Hotel[]  = [
     },
     social_info: {
        title: '@Delaware',
-       img: '/assets/images/r1.jpg',
+       img: 'assets/images/r1.jpg',
        followers: 15000,
        following: 9000
     },
